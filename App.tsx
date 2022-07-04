e1d44f17c60f09ae367c173f2e17f726ecb59174
chefo11 <101145786+chefo11@users.noreply.github.com>
2022-07-04 14:23:01 +0100

    still setting up auth

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './src/hooks/useCachedResources';
-import useColorScheme from './src/hooks/useColorScheme';
-import Navigation from './src/navigation';
+import useCachedResources from "./src/hooks/useCachedResources";
+import useColorScheme from "./src/hooks/useColorScheme";
+import Navigation from "./src/navigation";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
